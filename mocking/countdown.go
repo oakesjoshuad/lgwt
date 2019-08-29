f680916f8111a794a9ef9cf248da95c461398620
Joshua Oakes <joshua.d.oakes@gmail.com>
2019-08-29 13:39:35 -0700

    countdown

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,6 +10,8 @@ import (
 const (
 	finalWord      = "Go!"
 	countdownStart = 3
+	write          = "write"
+	sleep          = "sleep"
 )
 
 type Sleeper interface {
@@ -30,11 +32,23 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
-		fmt.Fprint(out, i)
-		fmt.Fprint(out, "\n")
+		fmt.Fprintln(out, i)
 	}
 	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
