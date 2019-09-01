042fc2fff2f7c4eac18bfb0313cfb3cc6eb67053
Joshua Oakes <joshua.d.oakes@gmail.com>
2019-09-01 16:11:18 -0700

    data modelling

--- appv2/server_test.go
+++ appv2/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -102,6 +103,11 @@ func TestLeague(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
 		response := httptest.NewRecorder()
 		server.ServeHTTP(response, request)
+		var got []Player
+		err := json.NewDecoder(response.Body).Decode(&got)
+		if err != nil {
+			t.Fatalf("unable to parse response from server %q into slice of Player, '%v'", response.Body, err)
+		}
 		assertStatus(t, response.Code, http.StatusOK)
 	})
 }
