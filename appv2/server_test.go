67e76701270050baebb2a17aaf02f700c283b1cb
Joshua Oakes <joshua.d.oakes@gmail.com>
2019-09-01 15:31:55 -0700

    added testing for league to server_test

--- appv2/server_test.go
+++ appv2/server_test.go
@@ -94,6 +94,18 @@ func TestRecordingWinsAndRetrievingThem(t *testing.T) {
 	assertResponseBody(t, response.Body.String(), "3")
 }
 
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+
 func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
